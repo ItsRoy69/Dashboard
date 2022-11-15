e2dd08c17b20b8a7daacfdc8b362d947d875dd3e
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2022-11-15 11:40:27 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React from 'react';
+
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import SkillTest from './components/Contents/SkillTest';
+
 
 function App() {
   return (
